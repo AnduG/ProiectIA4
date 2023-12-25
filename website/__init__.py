5f6d90701b3b2c7ef2ac4d8a65d490a79340ad85
Andrei Georgescu <andreigeorgescu03@gmail.com>
2023-12-25 12:03:56 +0200

    Databse Definition
    3 default users have been added to the database, John Doe, Joanne Doe, and Debby Debug, to be used for experimenting with features. their passwords are 1, 2 and 3, in that order.

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+from .views import views
+from .auth import auth
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -9,17 +11,12 @@ def create_app():
     BASE_DIR = path.abspath(path.dirname(__file__))
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'Super secret key'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + path.join(BASE_DIR, 'myapp.db')
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + path.join(BASE_DIR, 'database.db')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app);
 
-    from .views import views
-    from .auth import auth
-
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/auth/')
-
-    from .models import User, Statistics
 
     if not path.exists('website/' + DB_NAME):
         with app.app_context():
--- website/models.py
+++ website/models.py
@@ -1,6 +1,13 @@
 from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
+import enum
+
+class Activities(enum.Enum):
+    verbal_memory = 1
+    number_memory = 2
+    visual_memory = 3
+    reaction_time = 4
 
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
@@ -11,14 +18,14 @@ class User(db.Model, UserMixin):
     age = db.Column(db.Integer)
     first_name = db.Column(db.String(100), nullable=False)
     last_name = db.Column(db.String(100), nullable=False)
-
-    statisitcs = db.relationship('Statistics', back_populates='owner', uselist = True,
-                              cascade = 'all, delete-orphan, delete')
+    statistics = db.relationship('Statistics', back_populates='owner', uselist = True,
+                              cascade = 'all, delete-orphan, delete', lazy='dynamic')
 
 class Statistics(db.Model):
     __tablename__ = 'stats'
     id = db.Column(db.Integer, primary_key=True)
     owner_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    type = db.Column(db.Enum(Activities), nullable=False)
     results = db.Column(db.Integer)
     created_at = db.Column(db.DateTime(timezone=True), default=func.now())
     owner = db.relationship('User', back_populates='statistics')
