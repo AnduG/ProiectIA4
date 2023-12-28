886d7061fad6ed4909084c0fc018aeaa559aa2ec
paul_cristian.savu <paul.savuxx@yahoo.ca>
2023-12-28 13:44:12 +0200

    Created the game page, as well as the games' routes which must return a template specific to each game. Linked Play Button from homepage to gamepage.

--- website/views.py
+++ website/views.py
@@ -5,3 +5,32 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def homepage():
     return render_template('homepage.html')
+
+@views.route('/games')
+def games():
+    return render_template('gamepage.html')
+
+# To be completed with templates for each of the 6 games.
+@views.route('/games/reactionTime')
+def reactionTime():
+    return
+
+@views.route('/games/game2')
+def game2():
+    return
+
+@views.route('/games/game3')
+def game3():
+    return
+
+@views.route('/games/game4')
+def game4():
+    return
+
+@views.route('/games/game5')
+def game5():
+    return
+
+@views.route('/games/game6')
+def game6():
+    return
